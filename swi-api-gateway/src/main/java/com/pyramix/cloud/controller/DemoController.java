79d8749f9493c9684394dcd62e07f6dd891613aa
mrusli <mrusli@asimetris.co.id>
2026-04-27 06:21:37 +0700

    gateway to api signing

--- swi-api-gateway/src/main/java/com/pyramix/cloud/controller/DemoController.java
+++ swi-api-gateway/src/main/java/com/pyramix/cloud/controller/DemoController.java
@@ -1,18 +1,15 @@
 package com.pyramix.cloud.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * https://www.geeksforgeeks.org/advance-java/java-spring-boot-microservices-develop-api-gateway-using-spring-cloud-gateway/
  */
-@RestController
-@RequestMapping("/demo")
+// @RestController
+// @RequestMapping("/demo")
 public class DemoController {
 
-	@GetMapping("/gfg")
+	// @GetMapping("/gfg")
 	public ResponseEntity<String> getMessage() {
 		return ResponseEntity.ok("Welcome to GeeksforGeeks");
 	}
